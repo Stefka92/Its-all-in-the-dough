9d9024d78c70c3f5d7b124a0af3a1c15ac326f63
Stephanie McMahon <Smm200973@hotmail.com>
2023-07-03 18:22:25 +0000

    add app to allowed hosts

--- itsallinthedough/settings.py
+++ itsallinthedough/settings.py
@@ -34,7 +34,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['itsallinthedough.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['itsallinthedough-715c6b7f0f5c.herokuapp.com', 'local host']
+
 
 
 # Application definition
