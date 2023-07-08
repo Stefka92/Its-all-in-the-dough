e3a29771d3ef895d3441276ec7237482bcef536c
Stephanie McMahon <Smm200973@hotmail.com>
2023-07-08 14:52:30 +0000

    update models.py file with relavent fields

--- restaurantbookingsystem/models.py
+++ restaurantbookingsystem/models.py
@@ -4,23 +4,30 @@ from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
+from django.db import models
+
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
-    # Add other fields specific to the Restaurant model
+    address = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
 
 class Table(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
-    number = models.IntegerField()
+    table_number = models.IntegerField()
     capacity = models.IntegerField()
-    # Add other fields specific to the Table model
+
+    def __str__(self):
+        return f"Table {self.table_number} - {self.restaurant.name}"
 
 class Reservation(models.Model):
     table = models.ForeignKey(Table, on_delete=models.CASCADE)
-    customer = models.CharField(max_length=100)
-    date = models.DateField()
-    # Add other fields specific to the Reservation model
+    guest_name = models.CharField(max_length=100)
+    guest_email = models.EmailField()
+    reservation_date = models.DateField()
+    reservation_time = models.TimeField()
 
-class Customer(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    # Add other fields specific to the Customer model
+    def __str__(self):
+        return f"Reservation for {self.guest_name} at {self.table} on {self.reservation_date}"
